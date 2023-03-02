b0c0d5d7b3523969f328ba014794bcab15dc68b2
cloudhal <68559542+cloudhal@users.noreply.github.com>
2023-03-02 08:48:26 +0000

    0.2 Added license class file, creating list of objects and using them in license table

--- 365LicenseMonitor/Components/Licenses.razor.cs
+++ 365LicenseMonitor/Components/Licenses.razor.cs
@@ -5,11 +5,21 @@
         private bool _loading;
 
         private IEnumerable<Microsoft.Graph.SubscribedSku>? subscribedSkus;
+
+        // Create a list
+        List<License> LicenseList = new List<License> { };
         protected override async Task OnInitializedAsync()
         {
             subscribedSkus = await GraphClient.SubscribedSkus
            .Request()
            .GetAsync();
+
+            foreach (var sku in subscribedSkus)
+            {
+                // Create the license object and add to the list
+                 var lic = new License(sku.SkuId.ToString(), sku.SkuPartNumber, sku.ConsumedUnits, sku.PrepaidUnits.Enabled);
+                LicenseList.Add(lic);
+            }
         }
     }
 }
